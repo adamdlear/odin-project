04837dbfe8e306ab57729bef708f3bfd6c424827
Adam Lear <adamlear12@gmail.com>
2024-02-02 15:37:35 -0600

    complete js exercise 12

--- javascript-exercises/12_findTheOldest/findTheOldest.js
+++ javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson;
+    let highestAge = 0;
+    let age;
 
+    for (let person of people) {
+        if (person.yearOfDeath !== undefined) {
+            age = person.yearOfDeath - person.yearOfBirth;
+        } else {
+            age = 2024 - person.yearOfBirth;
+        }
+
+        if (age > highestAge) {
+            highestAge = age;
+            oldestPerson = person;
+        }
+    }
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
