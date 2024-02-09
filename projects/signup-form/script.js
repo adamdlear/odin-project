0fd6bf559ed69f65bf4040951569b93f37b5d90b
Adam Lear <adamlear12@gmail.com>
2024-02-08 23:24:05 -0600

    update form section

--- projects/signup-form/script.js
+++ projects/signup-form/script.js
@@ -1,6 +1,7 @@
 document.getElementById("form").addEventListener("submit", (e) => {
     if (document.getElementById("password").value != document.getElementById("confirm-password").value) {
         document.getElementById("password-span").textContent = "Passwords do not match";
+        console.log("passwords don't match");
         e.preventDefault();
     } else {
         document.getElementById("password-span").textContent = "";
