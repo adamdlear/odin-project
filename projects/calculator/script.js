c22ed20f724003395b1fa583a3e80c02ac4e29e2
Adam Lear <adamlear12@gmail.com>
2024-02-11 13:42:13 -0600

    update index.html

--- projects/calculator/script.js
+++ projects/calculator/script.js
@@ -1,8 +1,9 @@
-let X;
-let Y;
-let OP;
+let X = "";
+let Y = "";
+let OP = null;
 
-let inputString = "";
+const numberButtons = document.querySelector(".numberButtons");
+const operationButtons = document.querySelector("operationButtons");
 
 function add(x, y) {
     return x + y;
@@ -17,72 +18,25 @@ function multiply(x, y) {
 }
 
 function divide(x, y) {
-    return x / y;
-}
-
-function clearInput() {
-    const inputBar = document.querySelector(".inputBar");
-    inputBar.value = "";
-}
-
-function updateInputString(char) {
-    const inputBar = document.querySelector(".inputBar");
-    inputBar.value += char + " ";
-    inputString += char + " ";
-}
-
-function createNumberButtons() {
-    const container = document.querySelector(".numberButtons");
-
-    let number = 1;
-    for (let i = 0; i < 3; i++) {
-        const row = document.createElement("div");
-        row.classList.add("row");
-
-        for (let j = 0; j < 3; j++) {
-            const n = number;
-            const button = document.createElement("button");
-            button.textContent = `${n}`;
-            button.onclick = function() {
-                updateInputString(`${n}`);
-            };
-            button.classList.add("numberButton");
-            row.appendChild(button);
-            number++;
-        }
-
-        container.prepend(row);
-    }
-
-    const row = document.createElement("div");
-    row.classList.add("row");
-    const zero = document.createElement("button");
-    zero.textContent = "0";
-    zero.classList.add("numberButton");
-    zero.onclick = function() {
-        updateInputString("0");
-    };
-    row.appendChild(zero);
-    container.appendChild(row);
-}
-
-function parseInput() {
-    const split = inputString.split(" ");
-    X = parseInt(split[0]);
-    OP = split[1];
-    Y = parseInt(split[2]);
+    if (y === 0) return null;
+    else return x / y;
 }
 
 function operate(op = OP, x = X, y = Y) {
-    parseInput();
-    let output;
-    if (op === "+") output = add(x, y);
-    if (op === "-") output = subtract(x, y);
-    if (op === "x") output = multiply(x, y);
-    if (op === "/") output = divide(x, y);
+    x = Number(x);
+    y = Number(y);
 
-    const inputBar = document.querySelector(".inputBar");
-    inputBar.value = `${output}`;
+    switch (op) {
+        case "+":
+            return add(x, y);
+        case "-":
+            return subtract(x, y);
+        case "x":
+            return multiply(x, y);
+        case "/":
+            return divide(x, y);
+        default:
+            return null;
+    }
 }
-
-createNumberButtons();
+    
