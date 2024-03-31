132486be08423c156e8eff36eb5f81f9e1e4e73e
Adam Lear <adamlear12@gmail.com>
2024-03-31 12:51:58 -0500

    add basic styling

--- projects/library/scripts.js
+++ projects/library/scripts.js
@@ -1,4 +1,5 @@
-const tableBody = document.querySelector(".shelfBody");
+const tableBody = document.querySelector(".shelf-body");
+const submitButton = document.querySelector(".addBook");
 
 const myLibrary = [];
 
@@ -10,37 +11,51 @@ function Book(image, title, author, pages, read) {
     this.read = read;
 }
 
-function addBookToLibrary() {
+function createLibrary() {
     myLibrary.forEach((book) => {
         const row = document.createElement("tr");
+        row.classList.add("shelf-row");
 
         const image = document.createElement("img");
-        image.classList.add("bookCover")
+        image.classList.add("bookCover");
         image.src = book.image;
 
         const title = document.createElement("td");
         title.textContent = book.title;
-        // row.appendChild(title);
 
         const author = document.createElement("td");
         author.textContent = book.author;
-        // row.appendChild(author);
 
         const pages = document.createElement("td");
         pages.textContent = book.pages;
-        // row.appendChild(pages);
 
         const read = document.createElement("td");
         read.textContent = book.read;
-        // row.appendChild(read);
 
         row.append(image, title, author, pages, read);
         tableBody.append(row);
     });
 }
 
+function addBookToLibrary() {
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").value;
+
+    const book = new Book("test string", title, author, pages, read);
+    myLibrary.push(book);
+    clearShelf();
+    createLibrary();
+}
+
+function clearShelf() {
+    const tableRows = document.getElementsByClassName(".shelf-row");
+    tableRows.remove();
+}
+
 const testBook = new Book(
-    "images/martian.webp",
+    "images/tpp.jpg",
     "This Test Book",
     "Test Author",
     100,
@@ -55,6 +70,6 @@ const testBook1 = new Book(
 );
 
 myLibrary.push(testBook);
-// myLibrary.push(testBook1);
 
-addBookToLibrary();
+createLibrary();
+submitButton.addEventListener("click", addBookToLibrary());
